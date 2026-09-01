7b44a06d949fbcb06eacedf268830a9f99cddd84
dssierra9 <133552168+dssierra9@users.noreply.github.com>
2026-08-31 23:36:14 -0500

    agregado comparacion

--- app_nivel_cornare.py
+++ app_nivel_cornare.py
@@ -27,7 +27,7 @@ LAT_DEFECTO = 6.542
 LON_DEFECTO = -75.1576
 
 API_BASE_URL = "https://marco.cornare.gov.co/api/v1/estaciones"
-URL : /api/v1/estaciones/42/?format=api
+
 LLAVE_FECHA = "level_date"
 LLAVE_VALOR = "level"
 CANDIDATOS_LAT = ["lat", "latitude", "latitud"]
